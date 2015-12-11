31e4bbefe51474845a980941fd56a10553da58ac
dariaRWTH <khorkunova@gmail.com>
2015-12-11 23:03:18 +0100

    Get port from user, get IP with the help of library

--- PDSApp.py
+++ PDSApp.py
@@ -5,10 +5,13 @@ Created on Fri Dec 11 22:34:45 2015
 @author: Daria
 """
 
+import socket
+
 def main():
     print 'main main main'
     port = getPort()
     print 'Port is: ', port
+    getIP()
 
 
 def getPort():
@@ -18,9 +21,15 @@ def getPort():
     answer = raw_input('Change? (y\\n)\n')
     if answer == 'y':
         newPort = raw_input('Input correct port: ')
-    if newPort != -1:
-        return  newPort
-
+        if newPort != -1:
+            return  newPort
     return port
+
+def getIP():
+    ipAddress = None
+    hostname = socket.gethostname()
+    ipAddress = socket.gethostbyname(hostname)
+    print ipAddress
+
 
 if __name__ == "__main__": main()
